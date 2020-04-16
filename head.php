93ad474fd4bcea098380586a7e00b300268e9381
Tatsuya <tatsuya.hirayama.309@gmail.com>
2020-04-17 02:12:12 +0900

    second commit

--- head.php
+++ head.php
@@ -3,7 +3,7 @@
 
   <head>
   <meta charset="utf-8">
-  <title><?php echo $siteTitle; ?> タイトル決める</title>
+  <title><?php echo $siteTitle; ?> タイトル決める必要あり</title>
   <link rel="stylesheet" type="text/css" href="style.css">
   <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
